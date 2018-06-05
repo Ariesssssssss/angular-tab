3a64d186cc7a3b5a07562cfe7c3ce3ce32953761
lautumn <lautumn@lautumndeMacBook-Pro.local>
2018-06-05 21:21:02 +0800

    anglar-tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {NgZorroAntdModule} from 'ng-zorro-antd';
+import {SidebarComponent} from './layout/sidebar/sidebar.component';
+import {HeaderComponent} from './layout/header/header.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppRouterModule} from './app-router.module';
+import {SimpleReuseStrategy} from './service/SimpleReuseStrategy';
+import {RouteReuseStrategy} from '@angular/router';
+import {TabComponent} from './common/tab/tab.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    TabComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRouterModule,
+    NgZorroAntdModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: SimpleReuseStrategy }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
